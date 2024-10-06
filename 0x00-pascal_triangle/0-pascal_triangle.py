5302b6aaeea69f2233f3853efbbe235b42b46de6
chika-12 <markworship001@gmail.com>
2024-10-06 22:28:38 +0100

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    pascal = [[1]]
+    triangle = [[1]]
+
     for index in range(1, n):
         row = [1]
         for idx in range(1, index):
-            row.append(pascal[index - 1][idx - 1] + pascal[index - 1][idx])
+            row.append(triangle[index - 1][idx - 1] + triangle[index - 1][idx])
         row.append(1)
-        pascal.append(row)
-
-    return pascal
+        triangle.append(row)
+    return triangle
