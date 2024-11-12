050928b3fdab1173cabb79ab25085c493cb2d396
chika-12 <markworship001@gmail.com>
2024-11-12 03:28:28 +0100

    python

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lock boxes"""
+
+
+def canUnlockAll(boxes):
+    visited = set([0])
+    len_of_box = len(boxes)
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key not in visited and key < len_of_box:
+                visited.add(key)
+                stack.append(key)
+    if len_of_box == len(visited):
+        return True
+    else:
+        return False
